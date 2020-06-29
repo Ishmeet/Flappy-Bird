0324ff4675930bb65d02f2b4d62a81758fda85d1
Ishmeet Singh <Ishmeet.Singh1@ibm.com>
2020-06-29 23:59:50 +0530

    feat: minor changes dependency: none

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"image"
 	"image/color"
 	_ "image/jpeg"
+	"math/rand"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 const (
-	screenHeight = 640
-	screenWidth  = 480
-	tileSize     = 32
+	screenWidth      = 640
+	screenHeight     = 480
+	tileSize         = 32
+	pipeStartOffsetX = 8
+	pipeIntervalX    = 8
 )
 
 var tilesImage *ebiten.Image
@@ -21,16 +23,14 @@ var tilesImage *ebiten.Image
 type Game struct {
 	cameraX int
 	cameraY int
+
+	// Pipes
+	pipeTileYs []int
 }
 
 func init() {
-	// t, _ := os.Open("tiles.png")
-	// T, _, err := image.Decode(t)
-	// if err != nil {
-	// 	panic(err)
-	// }
 	var err error
-	tilesImage, _, err = ebitenutil.NewImageFromFile("tiles.png", ebiten.FilterDefault)
+	tilesImage, _, err = ebitenutil.NewImageFromFile("SomeTiles.png", ebiten.FilterDefault)
 	if err != nil {
 		panic(err)
 	}
@@ -41,6 +41,10 @@ func NewGame() *Game {
 	g := &Game{}
 	g.cameraX = 0
 	g.cameraY = 0
+	g.pipeTileYs = make([]int, 256)
+	for i := range g.pipeTileYs {
+		g.pipeTileYs[i] = rand.Intn(6) + 2
+	}
 	return g
 }
 
@@ -54,6 +58,17 @@ func floorDiv(x, y int) int {
 
 func floorMod(x, y int) int {
 	return x - floorDiv(x, y)*y
+}
+
+func (g *Game) pipeAt(tileX int) (tileY int, ok bool) {
+	if (tileX - pipeStartOffsetX) <= 0 {
+		return 0, false
+	}
+	if floorMod(tileX-pipeStartOffsetX, pipeIntervalX) != 0 {
+		return 0, false
+	}
+	idx := floorDiv(tileX-pipeStartOffsetX, pipeIntervalX)
+	return g.pipeTileYs[idx%len(g.pipeTileYs)], true
 }
 
 // Update ...
@@ -73,11 +88,21 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	op := ebiten.DrawImageOptions{}
 	for i := -2; i < nx+1; i++ {
 		op.GeoM.Reset()
-		op.GeoM.Scale(1, 1)
 		op.GeoM.Translate(float64(i*tileSize-floorMod(g.cameraX, tileSize)),
 			float64((ny-1)*tileSize-floorMod(g.cameraY, tileSize)))
-		// screen.DrawImage(tilesImage.SubImage(image.Rect(0, 0, tileSize, tileSize)).(*ebiten.Image), op)
-		screen.DrawImage(tilesImage.SubImage(image.Rect(0, 0, tileSize, tileSize)).(*ebiten.Image), &op)
+		screen.DrawImage(tilesImage, &op)
+
+		//pipe
+		if _, ok := g.pipeAt(floorDiv(g.cameraX, tileSize) + i); ok {
+			op := ebiten.DrawImageOptions{}
+			image, _ := ebiten.NewImage(20, 20, ebiten.FilterDefault)
+			op.GeoM.Reset()
+			op.GeoM.Scale(20, 20)
+			op.ColorM.Scale(62, 66, 46, 0.1)
+			op.GeoM.Translate(float64(i*tileSize-floorMod(g.cameraX, tileSize)),
+				float64(i*tileSize-floorMod(g.cameraY, tileSize)))
+			screen.DrawImage(image, &op)
+		}
 	}
 }
 
