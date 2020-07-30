8f1581ed0dc74d009839a750ae294e52c9a355bb
Ishmeet Singh <Ishmeet.Singh1@ibm.com>
2020-07-30 23:47:32 +0530

    feat: minor

--- main.go
+++ main.go
@@ -488,9 +488,7 @@ func (g *Game) bestScore() int {
 
 func (g *Game) drawClouds(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(float64(100+tileSize-floorMod(g.cameraX, tileSize)),
-		float64(tileSize-floorMod(g.cameraY, tileSize)))
-	// op.GeoM.Translate(100, 50)
+	op.GeoM.Translate(100, 50)
 	screen.DrawImage(cloud1Image, op)
 	op.GeoM.Reset()
 	op.GeoM.Translate(400, 100)
@@ -510,9 +508,8 @@ func (g *Game) drawFlappy(screen *ebiten.Image) {
 	if g.score > 0 && g.score%5 == 0 && cascadeAudioPlayer.IsPlaying() {
 		//op.GeoM.Rotate(float64(ir) * math.Pi / 10)
 		ir++
-	} else {
-		op.GeoM.Rotate(float64(g.vy16) / 96.0 * math.Pi / 6)
 	}
+	op.GeoM.Rotate(float64(g.vy16) / 96.0 * math.Pi / 6)
 	op.GeoM.Translate(float64(w)/2.0, float64(h)/2.0)
 	op.GeoM.Translate(float64(g.x16/16.0)-float64(g.cameraX), float64(g.y16/16.0)-float64(g.cameraY))
 	op.Filter = ebiten.FilterLinear
